77743628c14ba8c1fff33dc65c1d879009183664
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-22 23:39:15 +0530

    implement MenuItemRepository

---
+++ restaurant-service/src/main/java/com/rkisuru/restaurant/service/menuItem/MenuItemRepository.java
@@ -0,0 +1,11 @@
+package com.rkisuru.restaurant.service.menuItem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+    @Query("SELECT menuItem FROM MenuItem menuItem WHERE menuItem.menu.id = :menuId AND menuItem.id = :itemId")
+    MenuItem findMenuItemByMenuId(@Param("menuId") Long menuId, @Param("itemId") Long itemId);
+}
