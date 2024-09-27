e96bc42ea2f2ca32d45d584a4920b03106071520
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-27 18:01:10 +0530

    implement MenuType

---
+++ restaurant-service/src/main/java/com/rkisuru/restaurant/service/type/MenuType.java
@@ -0,0 +1,19 @@
+package com.rkisuru.restaurant.service.type;
+
+public enum MenuType {
+
+    ALL,
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    SNACK,
+    DESSERT,
+    BEVERAGE,
+    VEGETARIAN,
+    GLUTEN_FREE,
+    FAMILY,
+    REGIONAL,
+    HEALTHY,
+    SEASONAL,
+    SPECIAL
+}
