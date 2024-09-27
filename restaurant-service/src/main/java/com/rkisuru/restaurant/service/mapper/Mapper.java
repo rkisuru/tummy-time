35d9a828cfd5d64fc826c7d61cb951c93ed1371d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-27 18:11:02 +0530

    implement response mapping

--- restaurant-service/src/main/java/com/rkisuru/restaurant/service/mapper/Mapper.java
+++ restaurant-service/src/main/java/com/rkisuru/restaurant/service/mapper/Mapper.java
@@ -1,9 +1,12 @@
 package com.rkisuru.restaurant.service.mapper;
 
+import com.rkisuru.restaurant.service.file.ReadFile;
 import com.rkisuru.restaurant.service.menu.Menu;
 import com.rkisuru.restaurant.service.menu.MenuRequest;
+import com.rkisuru.restaurant.service.menu.MenuResponse;
 import com.rkisuru.restaurant.service.menuItem.MenuItem;
 import com.rkisuru.restaurant.service.menuItem.MenuItemRequest;
+import com.rkisuru.restaurant.service.menuItem.MenuItemResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,9 +24,31 @@ public class Mapper {
 
         return MenuItem.builder()
                 .name(request.name())
-                .description(request.description())
+                .consistOf(request.consistOf())
                 .price(request.price())
-                .category(request.category())
+                .build();
+    }
+
+    public MenuResponse toMenuResponse(Menu menu) {
+
+        return MenuResponse.builder()
+                .id(menu.getId())
+                .menuName(menu.getName())
+                .description(menu.getDescription())
+                .type(menu.getType())
+                .cover(ReadFile.readFile(menu.getCover()))
+                .menuItems(menu.getMenuItems())
+                .build();
+    }
+
+    public MenuItemResponse toMenuItemResponse(MenuItem menuItem) {
+
+        return MenuItemResponse.builder()
+                .id(menuItem.getId())
+                .name(menuItem.getName())
+                .image(ReadFile.readFile(menuItem.getImage()))
+                .consistOf(menuItem.getConsistOf())
+                .price(menuItem.getPrice())
                 .build();
     }
 }
