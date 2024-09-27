36844abf613a810c7ededd3ddc5b8cb2658df404
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-27 18:06:04 +0530

    implement validation

--- restaurant-service/src/main/java/com/rkisuru/restaurant/service/menu/MenuRequest.java
+++ restaurant-service/src/main/java/com/rkisuru/restaurant/service/menu/MenuRequest.java
@@ -1,8 +1,21 @@
 package com.rkisuru.restaurant.service.menu;
 
+import com.rkisuru.restaurant.service.type.MenuType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record MenuRequest(
 
+        @NotEmpty(message = "Name cannot be empty")
+        @NotNull(message = "Name cannot be null")
+        @Size(min = 2, max = 50, message = "Name must at least 2 characters long")
         String name,
-        String description
+
+        @NotEmpty(message = "Description cannot be empty")
+        @NotNull(message = "Description cannot be null")
+        String description,
+
+        MenuType type
 ) {
 }
--- restaurant-service/src/main/java/com/rkisuru/restaurant/service/menuItem/MenuItemRequest.java
+++ restaurant-service/src/main/java/com/rkisuru/restaurant/service/menuItem/MenuItemRequest.java
@@ -1,12 +1,23 @@
 package com.rkisuru.restaurant.service.menuItem;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 public record MenuItemRequest(
 
+        @NotEmpty(message = "Name cannot be empty")
+        @NotNull(message = "Name cannot be null")
+        @Size(min = 2, max = 50, message = "Name must at least 2 characters long")
         String name,
-        String description,
-        BigDecimal price,
-        String category
+
+        List<String> consistOf,
+
+        @NotEmpty(message = "Price cannot be empty")
+        @NotNull(message = "Price cannot be null")
+        BigDecimal price
 ) {
 }
