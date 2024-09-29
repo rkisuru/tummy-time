9e1b569d1083199a1b77cd22bff300b8de75da6b
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-29 12:28:48 +0530

    initialize payment service

---
+++ tummy-time-payment-service/src/test/java/com/rkisuru/tummytime/TummyTimePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkisuru.tummytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TummyTimePaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
