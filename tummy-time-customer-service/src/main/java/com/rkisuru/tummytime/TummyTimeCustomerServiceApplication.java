5b2b1f8e15d72686c1d6bff499f3fcae80ed784e
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-29 12:28:24 +0530

    initialize customer service

---
+++ tummy-time-customer-service/src/main/java/com/rkisuru/tummytime/TummyTimeCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkisuru.tummytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TummyTimeCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TummyTimeCustomerServiceApplication.class, args);
+	}
+
+}
