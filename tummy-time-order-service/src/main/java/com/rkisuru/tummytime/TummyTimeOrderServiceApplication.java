e41cbc62cf8c5ed7afe751ef00d53e0862668b79
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-29 12:28:31 +0530

    initialize order service

---
+++ tummy-time-order-service/src/main/java/com/rkisuru/tummytime/TummyTimeOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkisuru.tummytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TummyTimeOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TummyTimeOrderServiceApplication.class, args);
+	}
+
+}
